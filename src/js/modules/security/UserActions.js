28ee4c3771232b89bdbe26423d5066cc9dc4a4f2
Walden Raines <walden@subrosagames.com>
2013-04-10 21:13:33 -0400

    Whitespace changes.

--- src/js/modules/security/UserActions.js
+++ src/js/modules/security/UserActions.js
@@ -4,20 +4,20 @@
 // The loginToolbar directive is a reusable widget that can show login or logout buttons
 // and information the current authenticated user
 angular.module('security').directive('userActions', function (AuthenticationService) {
-        var directive = {
-            templateUrl: 'views/security/user-actions.html',
-            replace: true,
-            scope: true,
-            link: function ($scope) {
-                $scope.isAuthenticated = AuthenticationService.isAuthenticated;
-                $scope.login = AuthenticationService.showLogin;
-                $scope.logout = AuthenticationService.logout;
-                $scope.$watch(function () {
-                    return AuthenticationService.currentUser;
-                }, function (currentUser) {
-                    $scope.currentUser = currentUser;
-                });
-            }
-        };
-        return directive;
-    });
+    var directive = {
+        templateUrl: 'views/security/user-actions.html',
+        replace: true,
+        scope: true,
+        link: function ($scope) {
+            $scope.isAuthenticated = AuthenticationService.isAuthenticated;
+            $scope.login = AuthenticationService.showLogin;
+            $scope.logout = AuthenticationService.logout;
+            $scope.$watch(function () {
+                return AuthenticationService.currentUser;
+            }, function (currentUser) {
+                $scope.currentUser = currentUser;
+            });
+        }
+    };
+    return directive;
+});
