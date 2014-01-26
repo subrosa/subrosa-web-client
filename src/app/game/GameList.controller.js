b713d80447822ac8b413ce0cef410f238f9eadbb
Walden Raines <walden@subrosagames.com>
2014-01-26 12:49:13 -0500

    Add error handler to geolocation request for future error message.

--- src/app/game/GameList.controller.js
+++ src/app/game/GameList.controller.js
@@ -10,9 +10,18 @@
  *  Display the list of games.
  */
 angular.module('subrosa.game').controller('GameListController', function ($scope, geolocation, Game) {
+    var success, error;
     $scope.games = Game.query();
 
-    geolocation.getLocation().then(function (data) {
+    success = function (data) {
         $scope.coords = {lat: data.coords.latitude, long: data.coords.longitude};
-    });
+    };
+
+    error = function (data) {
+        // User rejected access to geolocation
+        // TODO: display some help about how they can allow it and why they should
+        $scope.coords = data;
+    };
+
+    geolocation.getLocation().then(success, error);
 });
