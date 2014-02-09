afdbc2183d93fc9fd11f24285621667d5a51d93b
Walden Raines <walden@subrosagames.com>
2014-02-08 20:30:37 -0500

    Adding fields match direct that tests that two fields match.

---
+++ src/app/forms/fieldsMatch.directive.js
@@ -0,0 +1,27 @@
+/**
+ * @ngdoc directive
+ * @name subrosa.forms:fieldsMatch
+ * *
+ * @description
+ *   Ensure two fields match.
+ */
+angular.module('subrosa.forms').directive('fieldsMatch', function () {
+    return {
+        restrict: 'A',
+        scope: true,
+        require: 'ngModel',
+        link: function (scope, elem, attrs, control) {
+            var checker = function () {
+                var field1, field2;
+
+                field1 = scope.$eval(attrs.ngModel);
+                field2 = scope.$eval(attrs.fieldsMatch);
+
+                return field1 === field2;
+            };
+            scope.$watch(checker, function (n) {
+                control.$setValidity("fieldsMatch", n);
+            });
+        }
+    };
+});
