a61596174cf810d5077725adfc090c99bbb8a027
Walden Raines <walden@subrosagames.com>
2014-02-22 14:06:23 -0500

    Adding some comments to the modal cache service.

--- src/app/components/modal/modalCache.service.js
+++ src/app/components/modal/modalCache.service.js
@@ -11,14 +11,33 @@
 angular.module('subrosa.components.modal').service('modalCache', function ($modal, $cacheFactory) {
     var modalCache = $cacheFactory('modal');
 
+    /**
+     * Put a modal templateUrl into the modal cache.
+     *
+     * @param modalId the id to store in the cache.
+     * @param templateUrl the templateUrl to store in the cache.
+     */
     this.put = function (modalId, templateUrl) {
         modalCache.put(modalId, templateUrl);
     };
 
+    /**
+     * Get the modal templateUrl from the modal cache.
+     *
+     * @param modalId the id to retrieve from the cache.
+     * @returns string the templateUrl of the modal.
+     */
     this.get = function (modalId) {
         return modalCache.get(modalId);
     };
 
+    /**
+     * Open the specified modal with the provided scope.
+     *
+     * @param modalId the modal to open.
+     * @param modalScope the scope to use for the modal.
+     * @returns Object the $modal object.
+     */
     this.openModal = function (modalId, modalScope) {
         var templateUrl = modalCache.get(modalId);
 
