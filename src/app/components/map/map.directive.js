66d989c24a44747aba3c582d021fdf31bc6cfc10
Walden Raines <walden@subrosagames.com>
2014-02-22 12:41:58 -0500

    Moving map directive controller into it's own file.

--- src/app/components/map/map.directive.js
+++ src/app/components/map/map.directive.js
@@ -1,46 +1,4 @@
 /**
- * @ngdoc controller
- * @name subrosa.components.map:MapController
- *
- * @requires $scope
- *
- * @description
- *  Controller for map directive which sets up some defaults on the $scope.
- */
-angular.module('subrosa.components.map').controller('MapController', function ($scope) {
-    var defaultCenter, defaultControls, defaultDrawControls, defaultOptions;
-
-    defaultCenter = {
-        lat: 30,
-        lng: -15,
-        zoom: 3
-    };
-
-    defaultControls = {};
-
-    defaultDrawControls = {
-        options: {
-            draw: {
-                circle: false,
-                marker: false,
-                polyline: false,
-                rectangle: false
-            }
-        }
-    };
-
-    defaultOptions = {};
-
-    $scope.center = defaultCenter;
-    $scope.controls = defaultControls;
-    $scope.options = defaultOptions;
-
-    if ($scope.allowEdit === true) {
-        $scope.controls.draw = defaultDrawControls;
-    }
-});
-
-/**
  * @ngdoc directive
  * @name subrosa.components.map:map
  *
