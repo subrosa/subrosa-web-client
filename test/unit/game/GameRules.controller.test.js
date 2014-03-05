792db38f552abbfd292954f84d39b411f7c8c861
Walden Raines <walden@subrosagames.com>
2014-03-04 19:16:12 -0500

    Adding a test for the game rules controller functionality.

--- test/unit/game/GameRules.controller.test.js
+++ test/unit/game/GameRules.controller.test.js
@@ -1,5 +1,22 @@
 describe('Controller: GameRulesController', function () {
-    beforeEach(module('subrosa.game'));
+    var $controller, $scope, MockGameZoneFactory;
 
-    it('does not do anything...yet.', function () {});
+    beforeEach(module('subrosa.game', 'mocks'));
+
+    beforeEach(inject(function (_$controller_, $rootScope, MockResource) {
+        $controller = _$controller_;
+        $scope = $rootScope.$new();
+        $scope.$stateParams = {gameUrl: 'raleigh-wars'};
+        MockGameZoneFactory = MockResource.$new();
+    }));
+
+    it("sets the game zones on the $scope.", function () {
+        spyOn(MockGameZoneFactory, "query").andCallThrough();
+        $controller('GameRulesController', {$scope: $scope, GameZone: MockGameZoneFactory});
+
+        expect(MockGameZoneFactory.query).toHaveBeenCalled();
+        expect($scope.gameZones).toEqual(MockGameZoneFactory.query());
+        expect($scope.gameZones.results.length).toBe(1);
+    });
 });
+
