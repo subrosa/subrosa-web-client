e7decb099a4872f6b8cb6c3d8bc76a3a68a2ebf1
Walden Raines <walden@subrosagames.com>
2014-05-07 19:51:23 -0400

    Adding publish button and action to game page.

--- src/app/game/Game.controller.js
+++ src/app/game/Game.controller.js
@@ -10,5 +10,22 @@
  *  Loads the game and sets up common game related functionality.
  */
 angular.module('subrosa.game').controller('GameController', function ($scope, Game) {
+    var success, error;
+
+    success = function () {
+        $scope.saving = false;
+    };
+
+    error = function (response) {
+        $scope.saving = false;
+        $scope.go('game.edit');
+        $scope.notifications = response.data.notifications;
+    };
+
     $scope.game = Game.get({url: $scope.$stateParams.gameUrl});
+
+    $scope.publishGame = function () {
+        $scope.saving = false;
+        $scope.game.$publish(success, error);
+    };
 });
--- test/unit/game/Game.controller.test.js
+++ test/unit/game/Game.controller.test.js
@@ -8,7 +8,14 @@ describe('Controller: Game', function () {
             get: function () {
                 return {
                     name: "Raleigh Wars",
-                    url: "raleigh-wars"
+                    url: "raleigh-wars",
+                    $publish: function (success, error) {
+                        if (this.failed) {
+                            error(this.errorResponse);
+                        } else {
+                            success(this.successResponse);
+                        }
+                    }
                 };
             }
         };
@@ -26,5 +33,37 @@ describe('Controller: Game', function () {
         expect(MockGameFactory.get).toHaveBeenCalledWith({url: 'raleigh-wars'});
         expect($scope.game.name).toBe(MockGameFactory.get().name);
         expect($scope.game.url).toBe(MockGameFactory.get().url);
+    });
+
+    describe('allows the publishing of a game.', function () {
+        beforeEach(function () {
+            spyOn($scope.game, '$publish').andCallThrough();
+        });
+
+        afterEach(function () {
+            expect($scope.game.$publish).toHaveBeenCalled();
+            expect($scope.saving).toBe(false);
+        });
+
+        it('and succeeds', function () {
+            $scope.publishGame();
+            expect($scope.notifications).not.toBeDefined();
+        });
+
+        it('and errors', function () {
+            $scope.go = function () {};
+            spyOn($scope, 'go');
+            $scope.game.failed = true;
+            $scope.game.errorResponse = {
+                data: {
+                    notifications: 'hi!'
+                }
+            };
+
+            $scope.publishGame();
+
+            expect($scope.go).toHaveBeenCalledWith('game.edit');
+            expect($scope.notifications).toBe('hi!');
+        });
     });
 });
