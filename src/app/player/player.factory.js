b3feda0b8c7c5f6d239adc6ba821949e84e001a5
Walden Raines <walden@subrosagames.com>
2014-12-05 12:06:33 -0500

    Use user player instead of account player for factory.

--- src/app/player/player.factory.js
+++ src/app/player/player.factory.js
@@ -8,7 +8,7 @@
  *  Provides a $resource for player(s).
  */
 angular.module('subrosa.player').factory('Player', function ($resource) {
-    return $resource('/subrosa/v1/account/:accountId/player/:id', {accountId: '@accountId', id: '@id'}, {
+    return $resource('/subrosa/v1/user/player/:id', {id: '@id'}, {
         query: {method: 'GET', isArray: false}
     });
 });
