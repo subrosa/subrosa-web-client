25670bc43a8f5c587a3ab9194ba2785d16ec7eda
Walden Raines <walden@subrosagames.com>
2015-04-05 00:35:13 -0400

    Add rudimentary account game list page.

--- test/unit/account/account-players.controller.test.js
+++ test/unit/account/account-players.controller.test.js
@@ -1,5 +1,5 @@
-describe('Controller: AccountPlayersController', function () {
-    var $controller, dependencies, $scope, Player;
+describe('Controller: AccountGamesController', function () {
+    var $controller, dependencies, $scope, User;
 
     beforeEach(module('subrosa.account', 'mocks'));
 
@@ -8,45 +8,21 @@ describe('Controller: AccountPlayersController', function () {
 
         $scope = $rootScope.$new();
 
-        Player = $resource;
-        $scope.account = $resource.get({id: 1});
+        User = $resource;
+        User.games = User.query;
 
         $controller = _$controller_;
-        dependencies = {$scope: $scope, Player: Player};
+        dependencies = {$scope: $scope, User: User};
     }));
 
-    describe("allows setting the current player", function () {
-        var player = {id: 1, name: 'walden'};
+    it("sets the current user's games on the $scope", function () {
+        var games = [1, 2, 3];
+        User.setSuccessResponse(games);
+        spyOn(User, 'games').andCallThrough();
 
-        beforeEach(function () {
-            $controller('AccountPlayersController', dependencies);
-            spyOn($scope.account, '$update').andCallThrough();
-        });
+        $controller('AccountGamesController', dependencies);
 
-        afterEach(function () {
-            expect($scope.account.$update).toHaveBeenCalled();
-        });
-
-        it("and succeed", function () {
-            $scope.setPlayer(player);
-            expect($scope.accountPlayerNotifications.length).toBe(0);
-        });
-
-        it("and fail", function () {
-            $scope.account.failed = true;
-            $scope.setPlayer(player);
-            expect($scope.accountPlayerNotifications.length).toBe(1);
-        });
-    });
-
-    it("sets the current user's players on the $scope", function () {
-        var players = [1, 2, 3];
-        Player.setSuccessResponse({results: players});
-        spyOn(Player, 'query').andCallThrough();
-
-        $controller('AccountPlayersController', dependencies);
-
-        expect(Player.query).toHaveBeenCalled();
-        expect($scope.players).toBe(players);
+        expect(User.games).toHaveBeenCalled();
+        expect($scope.games).toBe(games);
     });
 });
